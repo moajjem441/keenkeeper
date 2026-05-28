16295592786f1c79b6f83ed11e6cae20d29dda58
Moajjem Hossain <moajjem441@gmail.com>
2026-05-28 20:08:48 +0600

    footer added

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 import BannerPage from "@/components/Banner";
 
 import AllFriendsPage from "./all-friends/page";
+import FooterPage from "@/components/footer";
 
 export default function Home() {
   return (
     <div >
      <BannerPage></BannerPage>
      <AllFriendsPage></AllFriendsPage>
+     <FooterPage></FooterPage>
      </div>
   );
 }
